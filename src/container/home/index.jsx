2f7eeaffae73046b8042855c87d57e012408dd35
Laiba60 <laibasaeed026@gmail.com>
2025-06-18 09:03:01 +0500

    improve

--- src/container/home/index.jsx
+++ src/container/home/index.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
-
+import {useNavigate} from 'react-router-dom';
+import {Animate} from 'react-simple-animate';
+import './style.css';
 const Home = () => {
+  const navigate = useNavigate();
+  const handleNavigateToContactMepage=()=>{
+
+navigate('/contact');
+  }
   return (
-    <div>
-      
-    </div>
-  )
+    <section id="home" className="home">
+      <div className="home_text-wrapper">
+        <h1>
+          Hello, I'm Laiba.
+          <br />
+          Frontened developer
+        </h1>
+      </div>
+      <Animate
+      play
+      duration={1.5}
+      delay={1}
+      start={
+        {
+          transform:"translateY(550px)"
+        }
+      }
+      end={{
+        transform:"translateX(0px)"
+      }}
+>
+        <div className="home_contact-me">
+          <button onClick={handleNavigateToContactMepage}>Hire Me</button>
+        </div>
+      </Animate>
+    </section>
+  );
 }
 
 export default Home;
