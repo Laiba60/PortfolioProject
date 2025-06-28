5aebf4a65561f6a48751bd9c7d608c4006533a94
Laiba60 <laibasaeed026@gmail.com>
2025-06-28 12:04:40 +0600

    improve

---
+++ src/container/resume/utils.js
@@ -0,0 +1,33 @@
+ export const data={
+    experience:[
+        {
+        title:"Frontened Developer",
+        Description:"User experience ,Visual Design"
+        },
+        {
+            title:"Intern",
+            Description:"IT manager AT Trade Marketing center Lahore"
+        }
+    ],
+    education:[
+        {
+        title:"BS Software Enginnering",
+        Description:"Khawaja Fareed University of Engineering And Information Technology",
+        Marks:"3.76/4.00"
+        },
+        {
+            title:"ICS",
+            Description:"MTB College Sadiqabad",
+            Marks:"1087/1100"
+        },
+        {
+            title:"Matric",
+            Description:"Fatima Public High School Chack No 175/P ",
+            Marks:"1026/1100"
+        }
+
+    ]
+
+    
+   
+ }
