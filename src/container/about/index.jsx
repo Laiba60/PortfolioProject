237df015aaeb019a74f52bf9d4418491977464a7
Laiba60 <laibasaeed026@gmail.com>
2025-06-30 18:57:12 +0500

    improve

--- src/container/about/index.jsx
+++ src/container/about/index.jsx
@@ -6,13 +6,13 @@ import "./style.css";
 
 const personaldetails = [
   { label: "Name", value: "Laiba Saeed" },
-  { label: "Age", value: "27" },
+  { label: "Age", value: "21" },
   { label: "Address", value: "Pakistan" },
   { label: "Email", value: "laibasaeed026@gmail.com" },
 ];
 
 const jobsummary =
-  "Seasoned and independent Front End Developer with 5 years of experience in blending the art of design with skill of programming to deliver an immersive and engaging user experience through efficient website development, proactive feature optimization, and relentless debugging. Very passionate about aesthetics and UI design. It is imperative that you provide a thorough and professional approach to your resume. As a Front End Developer you'll be judged by your ability to use UX and UI concepts and follow design guidelines. It's about expressing your attention to detail and how you can help implement design ideas for your future employer.";
+  "Seasoned and independent Front End Developer with 2 years of experience in blending the art of design with skill of programming to deliver an immersive and engaging user experience through efficient website development, proactive feature optimization, and relentless debugging. Very passionate about aesthetics and UI design. It is imperative that you provide a thorough and professional approach to your resume. As a Front End Developer you'll be judged by your ability to use UX and UI concepts and follow design guidelines. It's about expressing your attention to detail and how you can help implement design ideas for your future employer.";
 
 const About = () => {
   return (
